d33e4027aee2d45fabb6bb184e5c008c392b5fba
GunterPearson <1715@holbertonschool.com>
2020-12-07 17:06:43 -0600

    pushing file 0-lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" lock Boxes"""
+
+
+def canUnlockAll(boxes):
+    """ function to check if box can be unlocked"""
+    new = [False for x in range(len(boxes))]
+    idx = []
+    nunl = []
+    for x in range(len(boxes)):
+        if x not in list(set(nunl)) and x != 0:
+            return False
+        for y in range(len(boxes[x])):
+            idx.append(boxes[x][y])
+        for z in idx:
+            for a in range(len(boxes[z])):
+                nunl.append(boxes[z][a])
+        nunl = nunl + idx
+    return True
