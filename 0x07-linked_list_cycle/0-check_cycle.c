f0a83f11b44881583964f0f9a501a9469c60ff29
GunterPearson <1715@holbertonschool.com>
2021-02-03 14:25:00 -0600

    pushing file 0-check_cycle and header file

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+/**
+ * check_cycle - check if list is a cycle
+ * @list: listint_t list
+ *
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+    const listint_t *temp;
+    int x;
+
+    temp = list;
+    x = 0;
+    while (temp != NULL)
+    {
+        if (x == 10)
+        {
+            return (1);
+        }
+        temp = temp->next;
+        x++;
+    }
+
+    return (0);
+}
