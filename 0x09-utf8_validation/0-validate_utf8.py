0e066ecab4d9861b843025e95738d1d8126be701
GunterPearson <1715@holbertonschool.com>
2021-02-17 16:26:41 -0600

    pushing file 0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,7 +4,20 @@
 
 def validUTF8(data):
     """ return based on valid"""
+    b = 0
+    m = 1 << 6
     for x in data:
-        if x > 128:
-            return False
-    return True
+        mask = 1 << 7
+        if b == 0:
+            while x & mask:
+                b += 1
+                mask = mask >> 1
+            if b > 4 or b == 1:
+                return False
+            if b == 0:
+                continue
+        else:
+            if not (x & mask and not (x & m)):
+                return False
+        b -= 1
+    return b == 0
