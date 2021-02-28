98e2535e15df14b9c6f8700b1b46a6abd5e41dab
GunterPearson <1715@holbertonschool.com>
2021-02-28 11:03:54 -0600

    pushing files 0-menger.c and header file

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,34 @@
+#include "menger.h"
+
+/**
+ * menger - print sponge
+ * @level: int representing level
+ *
+ * Return: void
+ */
+void menger(int level)
+{
+	int i, j, x, y, size;
+	char hash;
+
+	size = pow(3, level);
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			x = i;
+			y = j;
+			hash = '#';
+			while (x > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+					hash = ' ';
+				x = x / 3;
+				y = y / 3;
+			}
+			printf("%c", hash);
+		}
+		printf("\n");
+	}
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MEN_GER
+#define MEN_GER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif
