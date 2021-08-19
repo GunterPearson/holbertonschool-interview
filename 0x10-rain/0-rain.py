4382896570ce82c734d340a6e70d8752d8266a05
GunterPearson <1715@holbertonschool.com>
2021-08-19 08:14:25 -0500

    pushing file 0-rain.py

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""RAIN"""
+
+
+from operator import mul
+
+
+def rain(walls):
+    """Return: Integer indicating total amount of rainwater"""
+    len_x = walls[0]
+    multiply = 0
+    total = 0
+    set = 0
+    for num, x in enumerate(walls):
+        if num == 0 and x == 0:
+            continue
+        else:
+            if x != 0:
+                if set != 0 and set <= x:
+                    total += set * multiply
+                    multiply = 0
+                elif set != 0 and set > x:
+                    total += x * multiply
+                    multiply = 0
+                set = x
+            if x == 0:
+                multiply += 1
+    return total
