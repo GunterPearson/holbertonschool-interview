816296e0051694b1e0dc106d5ed48098e30087fb
GunterPearson <1715@holbertonschool.com>
2021-09-10 11:17:13 -0500

    pushing new file 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,48 +1,38 @@
 #!/usr/bin/python3
 """Count it module"""
 import requests
-import sys
 
 
-def count_words(subreddit, word_list, cont={}, next=None, rea={}):
-    """count words"""
-    headers = {"User-Agent": "nildiert"}
-    if next:
-        subr = requests.get('https://reddit.com/r/' + subreddit +
-                            '/hot.json?after=' + next, headers=headers)
+def count_words(subreddit, word_list, after=None, count={}):
+    """Count words in word_list in subreddit"""
+    if after is None:
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     else:
-        subr = requests.get('https://reddit.com/r/' + subreddit +
-                            '/hot.json', headers=headers)
-
-    if subr.status_code == 404:
-        return
-
-    if cont == {}:
+        url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+            subreddit, after)
+    subreddit_req = requests.get(url,
+                                 headers={"user-agent": "user"},
+                                 allow_redirects=False)
+    try:
+        data = subreddit_req.json().get("data")
+    except Exception as e:
+        return None
+    for word in word_list:
+        word = word.lower()
+        if word not in count.keys():
+            count[word] = 0
+    children = data.get("children")
+    for child in children:
+        top = (child.get("data").get("title").lower())
+        top = top.split(' ')
         for word in word_list:
-            cont[word] = 0
-            rea[word] = word_list.count(word)
-
-    subr_dict = subr.json()
-    data = subr_dict['data']
-    next = data['after']
-    subr_posts = data['children']
-
-    for post in subr_posts:
-        post_data = post['data']
-        post_title = post_data['title']
-        title_words = post_title.split()
-        for w in title_words:
-            for key in cont:
-                if w.lower() == key.lower():
-                    cont[key] += 1
-    if next:
-        count_words(subreddit, word_list, cont, next, rea)
+            word = word.lower()
+            count[word] += top.count(word)
+    after = data.get("after")
+    if after is None:
+        sorted_subs = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+        for i in sorted_subs:
+            if i[1] != 0:
+                print(i[0] + ": " + str(i[1]))
     else:
-        for key, val in rea.items():
-            if val > 1:
-                cont[key] *= val
-        sorted_abc = sorted(cont.items(), key=lambda x: x[0])
-        sorted_res = sorted(sorted_abc, key=lambda x: (-x[1], x[0]))
-        for res in sorted_res:
-            if res[1] > 0:
-                print('{}: {}'.format(res[0], res[1]))
+        return count_words(subreddit, word_list, after, count)
