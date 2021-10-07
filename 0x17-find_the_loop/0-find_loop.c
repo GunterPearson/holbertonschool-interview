c61f30bb02eaa199e7a59bb91f39f8c9dfdf5710
GunterPearson <1715@holbertonschool.com>
2021-10-07 12:10:43 -0500

    pushing files lists.h and 0-find_loop.c

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * find_listint_loop - Finds the loop in a linked list
+ * @head: Pointer
+ * Return: address or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+	return (NULL);
+}
