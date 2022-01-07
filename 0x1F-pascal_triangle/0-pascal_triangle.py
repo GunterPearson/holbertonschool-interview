361bd0d826c5c3c5086a449b247b8e15d3340ae0
GunterPearson <1715@holbertonschool.com>
2022-01-06 20:29:36 -0600

    pushing new file 0-pascal_triangle.py that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+pascal triangle
+"""
+
+def pascal_triangle(n):
+    """that returns a list of lists of integers representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
