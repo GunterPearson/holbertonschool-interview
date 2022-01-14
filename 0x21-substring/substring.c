f8ac41ba9fc84646ef316c4e9553ac36e0e8c641
GunterPearson <1715@holbertonschool.com>
2022-01-13 19:41:07 -0600

    pushing new file substring.c that find substring in string and header file

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,6 +1,65 @@
 #include "substring.h"
 
 /**
+ * compare - compares strings
+ * @s1: first string
+ * @i: index
+ * @s2: second string
+ *
+ * Return: length of the shorter word contained or 0 if no match
+ */
+int compare(char const *s1, unsigned int i, char const *s2)
+{
+	unsigned int j = 0;
+
+	for (j = 0; s1[i] && s2[j]; i++, j++)
+	{
+		if (s1[i] != s2[j])
+			return (0);
+	}
+	return (j);
+}
+
+/**
+ * set_to - sets elements of array
+ * @array: pointer to array
+ * @size: the size of the array
+ *
+ */
+void set_to(int **array, int size)
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+		(*array)[i] = -1;
+}
+
+/**
+ * mem - allocates memory for int arrays
+ * @word_used: pointer to first
+ * @nb_words: number of words
+ * @indices: pointer to second
+ * @max_index: max number
+ *
+ * Return: -1 or 0
+ */
+int mem(int **word_used, int nb_words, int **indices, int max_index)
+{
+	*word_used = malloc(sizeof(int) * nb_words);
+	if (*word_used == NULL)
+		return (-1);
+	set_to(word_used, nb_words);
+	*indices = malloc(sizeof(int) * max_index);
+	if (*indices == NULL)
+	{
+		free(word_used);
+		return (-1);
+	}
+	set_to(indices, max_index);
+	return (0);
+}
+
+/**
 * find_substring - find substring in string
 * @s: string
 * @words: words to find
@@ -11,43 +70,45 @@
 */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    int *indices;
-    int i, j, k, l;
+	int word = 0, i = 0, match = 0, count = 0, j = 0, k = 0, I = 0;
+	int *word_used = NULL, *indices = NULL;
 
-    indices = malloc(sizeof(int) * nb_words);
-    if (!indices)
-        return (NULL);
-    for (i = 0; i < nb_words; i++)
-    {
-        for (j = 0; s[j] == words[i][j]; j++)
-        {
-            if (words[i][j + 1] == '\0')
-            {
-                indices[i] = j;
-                break;
-            }
-        }
-        if (j == 0)
-        {
-            free(indices);
-            return (NULL);
-        }
-    }
-    for (i = 0; i < nb_words; i++)
-    {
-        for (j = i + 1; j < nb_words; j++)
-        {
-            if (indices[i] > indices[j])
-            {
-                k = indices[i];
-                indices[i] = indices[j];
-                indices[j] = k;
-                l = i;
-                i = j;
-                j = l;
-            }
-        }
-    }
-    *n = nb_words;
-    return (indices);
+	*n = 0;
+	if (words == NULL)
+		return (NULL);
+
+	j = (nb_words * strlen(words[0]));
+	i = mem(&word_used, nb_words, &indices, (strlen(s) / j) + 1);
+	if (i == -1)
+		return (NULL);
+	for (i = 0; s[i]; i++)
+	{
+		I = i;
+		for (set_to(&word_used, nb_words), word = 0; word < nb_words && s[i]; word++)
+		{
+			match = compare(s, i, words[word]);
+			while (match != 0)
+			{
+				word_used[word] = 1;
+				i += match;
+				if (s[i] == '\0')
+					break;
+				for (count = 0, word = 0; word < nb_words; word++)
+				{
+					if (word_used[word] != -1)
+						count++;
+					match = compare(s, i, words[word]);
+					if (match != 0)
+						break;
+				}
+				if (count == nb_words)
+					indices[k++] = (i - j);
+			}
+		}
+		if (i != I || s[i] == '\0')
+			i--;
+	}
+	free(word_used);
+	*n = k;
+	return (indices);
 }
--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -3,5 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
 #endif
