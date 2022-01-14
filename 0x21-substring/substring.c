389f70278cd106e59a3a609ccb1447829fd63c4c
GunterPearson <1715@holbertonschool.com>
2022-01-13 19:49:58 -0600

    pushing new file substring.c that find substring in string and header file

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,62 +1,78 @@
 #include "substring.h"
 
-/**
- * compare - compares strings
- * @s1: first string
- * @i: index
- * @s2: second string
- *
- * Return: length of the shorter word contained or 0 if no match
- */
-int compare(char const *s1, unsigned int i, char const *s2)
-{
-	unsigned int j = 0;
-
-	for (j = 0; s1[i] && s2[j]; i++, j++)
-	{
-		if (s1[i] != s2[j])
-			return (0);
-	}
-	return (j);
-}
 
 /**
- * set_to - sets elements of array
- * @array: pointer to array
- * @size: the size of the array
- *
+ * free_tree - frees nodes
+ * @root: pointer
  */
-void set_to(int **array, int size)
+void free_tree(Tree *root)
 {
 	int i = 0;
 
-	for (i = 0; i < size; i++)
-		(*array)[i] = -1;
+	for (i = 0; i < 26; i++)
+		if (root->children[i])
+			free_tree(root->children[i]);
+	free(root);
 }
 
 /**
- * mem - allocates memory for int arrays
- * @word_used: pointer to first
- * @nb_words: number of words
- * @indices: pointer to second
- * @max_index: max number
- *
- * Return: -1 or 0
+ * tree - searches for word
+ * @node: root of tree
+ * @str: string
+ * @k: characters in string
+ * @memo: array
+ * @j: index
+ * Return: 1 or 0
  */
-int mem(int **word_used, int nb_words, int **indices, int max_index)
+int tree(Tree *node, char *str, int k, Tree **memo, int j)
 {
-	*word_used = malloc(sizeof(int) * nb_words);
-	if (*word_used == NULL)
-		return (-1);
-	set_to(word_used, nb_words);
-	*indices = malloc(sizeof(int) * max_index);
-	if (*indices == NULL)
+	for (; k; str++, k--)
 	{
-		free(word_used);
-		return (-1);
+		if (!node->children[INDEX(*str)])
+			return (0);
+		node = node->children[INDEX(*str)];
 	}
-	set_to(indices, max_index);
+
+	memo[j] = node;
+	if (node->left-- > 0)
+	{
+		return (1);
+	}
 	return (0);
+}
+
+/**
+ * build_tree - builds tree
+ * @root: pointer
+ * @words: pointer
+ * @nb_words: number of words
+ * @nodes: populate array
+ */
+void build_tree(Tree *root, char const **words, int nb_words, Tree **nodes)
+{
+	int i = 0;
+	char const *str;
+	Tree *node;
+
+	for (i = 0; i < nb_words; i++)
+	{
+		node = root;
+		for (str = words[i]; *str; str++)
+		{
+			if (!node->children[INDEX(*str)])
+			{
+				node->children[INDEX(*str)] = calloc(1, sizeof(Tree));
+				if (!node->children[INDEX(*str)])
+					exit(1);
+			}
+			node = node->children[INDEX(*str)];
+		}
+		node->word = 1;
+		node->count++;
+		node->left++;
+		node->val = (char *)words[i];
+		nodes[i] = node;
+	}
 }
 
 /**
@@ -70,45 +86,44 @@ int mem(int **word_used, int nb_words, int **indices, int max_index)
 */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int word = 0, i = 0, match = 0, count = 0, j = 0, k = 0, I = 0;
-	int *word_used = NULL, *indices = NULL;
+	int i = 0, j, k, slen, matches;
+	Tree *root, **nodes, **memo;
+	int *indices;
 
 	*n = 0;
-	if (words == NULL)
-		return (NULL);
+	slen = strlen(s);
+	k = strlen(words[0]);
+	indices = calloc(slen, sizeof(int));
+	root = calloc(1, sizeof(Tree));
+	nodes = calloc(nb_words, sizeof(*nodes));
+	memo = calloc(slen, sizeof(*memo));
+	if (!indices || !root || !nodes || !memo)
+		exit(1);
+	build_tree(root, words, nb_words, nodes);
 
-	j = (nb_words * strlen(words[0]));
-	i = mem(&word_used, nb_words, &indices, (strlen(s) / j) + 1);
-	if (i == -1)
-		return (NULL);
-	for (i = 0; s[i]; i++)
+	for (i = 0; i < slen; i++)
 	{
-		I = i;
-		for (set_to(&word_used, nb_words), word = 0; word < nb_words && s[i]; word++)
+		matches = 0;
+		for (j = i; j <= slen - k; j += k)
 		{
-			match = compare(s, i, words[word]);
-			while (match != 0)
+			if ((memo[j] && memo[j]->left-- > 0) ||
+				tree(root, (char *)s + j, k, memo, j))
 			{
-				word_used[word] = 1;
-				i += match;
-				if (s[i] == '\0')
-					break;
-				for (count = 0, word = 0; word < nb_words; word++)
+				if (++matches == nb_words)
 				{
-					if (word_used[word] != -1)
-						count++;
-					match = compare(s, i, words[word]);
-					if (match != 0)
-						break;
+					indices[*n] = i;
+					*n += 1;
+					break;
 				}
-				if (count == nb_words)
-					indices[k++] = (i - j);
 			}
+			else
+				break;
 		}
-		if (i != I || s[i] == '\0')
-			i--;
+		for (j = 0; j < nb_words; j++)
+			nodes[j]->left = nodes[j]->count;
 	}
-	free(word_used);
-	*n = k;
+	free_tree(root), free(nodes), free(memo);
+	if (*n == 0)
+		indices = (free(indices), NULL);
 	return (indices);
 }
--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -4,6 +4,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#define INDEX(x) ((x) - 'a')
+
+/**
+ * struct Tree - trie struct
+ * @children: array of child nodes
+ * @word: 1 if word
+ * @count: instances
+ * @left: instances left
+ * @val: pointer
+ */
+typedef struct Tree
+{
+	struct Tree *children[26];
+	int word;
+	int count;
+	int left;
+	char *val;
+} Tree;
 
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 #endif
