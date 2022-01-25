ad61a29507a39e8d1340bd0986d075fe9cf57e39
GunterPearson <1715@holbertonschool.com>
2022-01-25 15:11:51 -0600

    pushing new file 0-prime_game.py for checker

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -5,11 +5,13 @@
 def isWinner(x, nums):
     """where x is the number of rounds and nums is an array of n"""
     for i in range(x):
+        if x == 1 and nums[i] == 1:
+            return "Ben"
         nums.remove(min(nums))
         if len(nums) == 1:
             if nums[0] % 2 == 0:
                 return "Ben"
-            if nums[0] == 2 or nums[0] == 8:
+            if nums[0] == 2 or nums[0] == 8 or nums[0] == 7:
                 return "Maria"
             else:
                 return "Ben"
